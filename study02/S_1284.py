79a2ecc759c537ad8aaeee7b1e2911605848a792
dain jung <91936267+wannabedain@users.noreply.github.com>
2024-07-22 21:54:54 +0900

    수도 요금 경쟁 / 8분

--- study02/S_1284.py
+++ study02/S_1284.py
@@ -2,20 +2,39 @@
 
 T = int(input())
 
-P, Q, R, S, W = map(int, input().split())
-
 for tc in range(1, T+1):
+    
+    P, Q, R, S, W = map(int, input().split())
     
     # A사 
     a = P * W
 
     # B사   1) R리터 초과인 경우 2) R리더 이하인 경우
     if W > R : 
-        b = R * Q + S * (W-Q)
+        b = Q + S * (W-R)
     else:
-        b = W * Q
+        b = Q
         
     if a < b : ans = a
     else : ans = b
 
     print(f'#{tc} {ans}')
+
+
+
+
+
+'''
+T = int(input())
+
+for test_case in range(1, T+1) :
+    P, Q, R, S, W = map(int, input().split())
+    a = P*W
+    if W <= R:
+        b= Q
+    else: b= Q + (W-R)*S
+    
+    if a>b : print(f'#{test_case} {b}') 
+    else : print(f'#{test_case} {a}')
+
+'''
