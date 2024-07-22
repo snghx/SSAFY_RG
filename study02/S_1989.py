4d2ef58691bfe512b9e07a5c1a39cee174c1176d
dain jung <91936267+wannabedain@users.noreply.github.com>
2024-07-22 21:54:00 +0900

    초심자의 회문 검사 / 6분

--- study02/S_1989.py
+++ study02/S_1989.py
@@ -5,7 +5,10 @@ T = int(input())
 for tc in range(1, T+1):
     S = str(input())
 
-    if S[:] == S[::-1]:
+    if S[:] == S[::-1]:   # 문자열 뒤로 읽기 [::-1]
         print(f'#{tc} 1')
     else:
         print(f'#{tc} 0')
+
+
+# 만약 level[::-2]라면 lvl
