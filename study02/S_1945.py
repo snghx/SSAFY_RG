a9838aa427654cdf20bf6d2d948d7246baefaeb4
dain jung <91936267+wannabedain@users.noreply.github.com>
2024-07-22 20:28:38 +0900

    간단한 소인수분해 / 5분

---
+++ study02/S_1945.py
@@ -0,0 +1,38 @@
+# 간단한 소인수분해
+
+T = int(input())
+
+for tc in range(1, T+1):
+
+    N = int(input())
+
+    print(f'#{tc}', end = ' ')
+    
+    # prime factorization 리스트 만들기
+    pf = [2, 3, 5, 7, 11]
+
+    for p in pf:
+        cnt = 0
+        while N % p == 0:
+            N //= p
+            cnt += 1
+
+        print(cnt, end = ' ')
+
+    print()
+   
+
+
+'''
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    print('#{}'.format(test_case), end=' ')
+    for i in [2,3,5,7,11]:
+        cnt = 0
+        while N % i == 0:
+            N //= i
+            cnt += 1
+        print(cnt, end=' ')
+    print()
+'''
