af058f4d7d556f20530b543a09a75c13b81b9760
snghx <nho0814@sookmyung.ac.kr>
2024-07-22 21:07:33 +0900

    새로운 불면증 치료법 / 20분

--- study02/s_1288.py
+++ study02/s_1288.py
@@ -1,19 +1,18 @@
-# 1288. 새로운 불면증 치료법
+# 1288. 새로운 불면증 치료법 9:02 시작
 
 T = int(input())
 
 for t in range(1, T+1):
     num = int(input())
-    num_set = {}
+    num_set = set()
     cnt = 1
     
-    while len(num_set) != 10:
-        for n in str(num):
-            print(n)
-            num_set.update(n)
+    while len(num_set) < 10:
+        temp = cnt * num
+        
+        for s in str(temp):
+            num_set.add(s)
             
-        print(num_set)
-        num = int(num) * cnt
         cnt += 1
     
-    print(f"#{t} {cnt}")
+    print(f"#{t} {temp}")
