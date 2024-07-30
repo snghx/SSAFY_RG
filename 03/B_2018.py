7019bb75e446061daf0dc0ca12d8088ad83b13b1
hyemin <hyemin3699@naver.com>
2024-07-30 18:44:51 +0900

    B_2018.py

---
+++ 03/B_2018.py
@@ -0,0 +1,17 @@
+# 2018. 수들의 합 
+
+n= int(input())
+
+# 입력 받은 수를 활용하여 리스트 생성
+n_list = []
+for i in range(1, n+1) :
+    n_list.append(i)
+
+# start_index와 end_incex 모두 1로 지정 
+s = 1
+e = 1
+
+
+cnt = 1
+
+
